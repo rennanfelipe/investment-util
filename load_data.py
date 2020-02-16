576350b82290f06b381266856f5c09d3fb65849b
rennanfelipe <rennan.chaves95@gmail.com>
2020-02-16 16:47:00 -0300

    Created project basic structure

---
+++ load_data.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+class laodData:
+    def __init__(self):
+        #load files into program
+        pass
---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+if __name__ == "__main__":
+    # get script inputs
+    # load investment data
+    
+
+    # calculate investments
+    # generate graphs
+    # generate PDF
+    pass
